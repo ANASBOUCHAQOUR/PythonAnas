72e5d463073bfaa8a2a155b8a950cba61afa5b28
ANASBOUCHAQOUR <bouchaqouranas@gmail.com>
2025-05-14 15:01:03 +0100

    Update Week 2 Day 2 Daily Challenge - Change Answer to output

--- Week 2/DAY 2/Daily challenge/Daily-challenge.sql
+++ Week 2/DAY 2/Daily challenge/Daily-challenge.sql
@@ -2,7 +2,7 @@
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft 
 -- WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NULL);
--- Answer: 0
+-- output: 0
 -- The subquery returns NULL. `NOT IN (NULL)` causes all comparisons to return UNKNOWN,
 -- so no rows match the condition. Result is 0.
 
@@ -10,14 +10,14 @@
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft 
 -- WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id = 5);
--- Answer: 2
+-- output: 2
 -- subquery returns (5), so rows with id 6 and 7 are included. NULL is ignored in `NOT IN`.
 
 -- Q3
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft 
 -- WHERE ft.id NOT IN (SELECT id FROM SecondTab);
--- Answer: 0
+-- output: 0
 -- The subquery returns (5, NULL). `NOT IN` with NULL in the list results in UNKNOWN,
 -- so no rows satisfy the condition. Result is 0.
 
@@ -25,5 +25,5 @@
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft 
 -- WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NOT NULL);
--- Answer: 2
+-- output: 2
 -- The subquery returns (5). So rows with id 6 and 7 match. NULL is not compared.
